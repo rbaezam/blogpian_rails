8c61853cddcca5956d2a194d383822f489feb44e
Rodolfo Baeza <rbaezam@gmail.com>
2015-06-01 09:53:01 -0500

    Added caching to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ class PostsController < ApplicationController
   # before_action :authenticate_user!
   respond_to :html, :json
 
+  caches_action :index, :show
+
   def index
     @posts = Post.all.desc(:created_at)
     respond_with(@posts)
@@ -19,6 +21,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.tags = @post.tags_string.split(',')
     if @post.save
+      expire_action :action => :index
       redirect_to root_path
     else
       render :new
