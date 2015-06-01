0f58d3282b32f184a2a7ea949b85a68df15c12cd
Rodolfo Baeza <rbaezam@gmail.com>
2015-06-01 11:25:38 -0500

    Added and removed some tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  before(:each) do
+    Post.delete_all
+    @post1 = Post.new(title: 'first post')
+    @post1.save
+  end
 
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "returns JSON string with posts" do
+    end
   end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, id: @post1.id
       expect(response).to have_http_status(:success)
     end
   end
@@ -24,31 +32,15 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    it "returns http found" do
+      post :create, {post:{title:'new post', body:'body for the new post',tags_string:'technology'}}
+      expect(response).to have_http_status(:found)
+    end
+
+    it "creates a new post" do
+      post :create, {post:{title:'new post', body:'body for the new post',tags_string:'technology'}}
+      newPost = Post.last
+      expect(newPost.title).to eq('new post')
     end
   end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
